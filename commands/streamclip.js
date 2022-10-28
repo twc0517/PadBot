9c2bf4066a8cd72f641ac1c952b736e7919d0e77
twc0517 <twc0517@gmail.com>
2022-10-27 22:21:09 -0400

    youtube.com option

--- commands/streamclip.js
+++ commands/streamclip.js
@@ -1,19 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
-const screenshotApiClient = require('screenshotapi.net')('618N9Q1-7F9M6XB-K521FBF-CB8G26V');
+const { SlashCommandBuilder, Attachment } = require('discord.js');
+var Youtube = require('youtube.com');
+
+var youtube = Youtube('https://youtu.be/Tk338VXcb24');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('streamclip')
 		.setDescription('Gets the current frame of a livestream'),
 	async execute(interaction) {
-		screenshotApiClient.saveScreenshotToImage(`screenshot.png`, {
-            url: 'https://www.youtube.com/watch?v=mhJRzQsLZGg&list=PLM5LLrSdx9qxa8Gr7yGsm00hazeChiqsd&index=1&ab_channel=NASASpaceflight',
-            width: 1920,
-            height: 1080,
-        })
-        .catch((error) => {
-            console.error("Error while getting screenshot.");
-            console.dir(error);
-        })
+        await interaction.deferReply();
+		youtube.snapshot('0:05', './screenshot.jpg')
+        .then(function () {
+            console.log("Done");
+        }).catch(function (err) {
+            console.log("err : ", err)
+        });
+        await interaction.editReply({files: ['./screenshot.jpg']});
 	},
 };
