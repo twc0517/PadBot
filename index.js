029906a00a08c43102f73dc2e02858a1fc7c9a87
twc0517 <84995579+twc0517@users.noreply.github.com>
2022-11-02 17:18:13 +0000

    Merging streamclip-development into main

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const client = new Client({
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
         IntentsBitField.Flags.GuildMembers,
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> streamclip-development
     ]
 })
 
