a918524ed444d1c88f757411f5f8d305fe37446e
twc0517 <84995579+twc0517@users.noreply.github.com>
2022-11-02 17:03:33 +0000

    Commented profile changes on streamupdate

--- commands/streamupdate.js
+++ commands/streamupdate.js
@@ -62,12 +62,12 @@ module.exports = {
         var message = interaction.options.getString('message');
         console.log(streamChoice);
         console.log(urlChoice);
-        var avatarURL = interaction.member.displayAvatarURL();
+        /*var avatarURL = interaction.member.displayAvatarURL();
         var guild = client.guilds.fetch('1033370495711715359');
         console.log(guild);
         var clientMember = guild.members.get(client.user.fetch());
         console.log(clientMember);
-        var nickname = interaction.member.displayName;
+        var nickname = interaction.member.displayName;*/
 
         if (streamChoice == null && urlChoice == null){
             await interaction.reply('You must either choose a 24/7 stream or input a livestream URL');
@@ -91,12 +91,12 @@ module.exports = {
                 .pipe(videoFile);
             setTimeout(getFrame, 10000);    
             await new Promise(resolve => setTimeout(resolve, 11000));
-            client.user.setAvatar(avatarURL);
-            clientMember.setNickname(nickname);
+            //client.user.setAvatar(avatarURL);
+            //clientMember.setNickname(nickname);
             await interaction.editReply({content: message, files: ['./frame.png']});
             await interaction.channel.send(photoCredit);
-            client.user.setAvatar('./botAvatar.jpg');
-            clientMember.setNickname('PadBot');
+            //client.user.setAvatar('./botAvatar.jpg');
+            //clientMember.setNickname('PadBot');
             fs.unlink('./frame.png', (err) => {
                 if (err) {
                   console.error(err)
